fd32795525bca18347eef9339fbad7c6fb6c3e00
abtinokhovat <abtinokhovat@gmail.com>
2022-12-25 20:38:24 +0330

    add plop generator

---
+++ plopfile.js
@@ -0,0 +1,34 @@
+module.exports = function (plop) {
+  plop.setGenerator("component", {
+    description: "application component logic",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Logic Component name please",
+      },
+      {
+        type: "input",
+        name: "entity",
+        message: "Entity name please",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: `components/{{pascalCase name}}/{{lowerCase name}}.vhd`,
+        templateFile: "templates/main.vhd.hbs",
+      },
+      {
+        type: "add",
+        path: `components/{{pascalCase name}}/{{lowerCase name}}_tb.vhd`,
+        templateFile: "templates/tb.vhd.hbs",
+      },
+      {
+        type: "add",
+        path: `components/{{pascalCase name}}/Makefile`,
+        templateFile: "templates/makefile.hbs",
+      },
+    ],
+  });
+};
