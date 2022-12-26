7cc62da521935cb72501807c61e74f70b0f48f30
abtinokhovat <abtinokhovat@gmail.com>
2022-12-26 11:11:08 +0330

    add ParityChecker

--- components/ParityChecker/paritychecker.vhd
+++ components/ParityChecker/paritychecker.vhd
@@ -3,10 +3,29 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 entity checker is
- port();
+    generic (
+        size: integer := 8
+    );
+     port(
+        parity: in std_logic;
+        input: in std_logic_vector(size - 1 downto 0);
+        error_even: out std_logic;
+        error_odd: out std_logic
+     );
 end checker;
  
 architecture behavior of checker is
 begin
+    process(input,parity)
+        variable temp: std_logic;  
+    begin
+        temp := '0';
+        for i in 0 to input'length - 1 loop
+            temp := temp xor input(i);
+        end loop;
+
+        error_even <= temp xor parity;
+        error_odd <= not (temp xor parity);
+    end process;
 end behavior;
 
--- components/ParityChecker/paritychecker_tb.vhd
+++ components/ParityChecker/paritychecker_tb.vhd
@@ -4,13 +4,80 @@ use ieee.std_logic_1164.all;
 entity tb_checker is
 end entity;
 
-architecture behavioral of tb_checker is
-    generic();
+architecture behavior of tb_checker is
     component checker is
+        generic (
+            size: integer := 8
+        );
+        port(
+            parity: in std_logic;
+            input: in std_logic_vector(size - 1 downto 0);
+            error_even: out std_logic;
+            error_odd: out std_logic
+        );
     end component checker;
+
+    -- signals here
+    signal input: std_logic_vector(7 downto 0);
+    signal parity: std_logic;
     
+    signal error_even: std_logic;
+    signal error_odd: std_logic;
+    
+begin
+    uut: checker port map(
+        parity => parity,
+        input => input,
+        error_even => error_even,
+        error_odd => error_odd
+    );
+    process
     begin
-        uut: checker port map();
-    end;
-end behavioral;
+        -- exhaustive test
+        -- for i in 0 to (2 ** input'length) / 40 loop
+        --     input <= std_logic_vector(to_unsigned(i * 40, input'length));       
+
+        --     parity <= '0';
+        --     wait for 10 ns;
+
+        --     parity <= '1';
+        --     wait for 10 ns;
+        -- end loop ;  
+
+        input <= "01001101";
+        parity <= '1';       
+        wait for 10 ns;
+
+        input <= "00010011";
+        parity <= '1';       
+        wait for 10 ns;
+
+        input <= "11011101";
+        parity <= '1';       
+        wait for 10 ns;
+
+        input <= "01111101";
+        parity <= '0';       
+        wait for 10 ns;
+
+        input <= "01010100";
+        parity <= '1';       
+        wait for 10 ns;
+
+        input <= "10101110";
+        parity <= '0';       
+        wait for 10 ns;
+
+        input <= "11001010";
+        parity <= '1';       
+        wait for 10 ns;
+    wait;
+    end process;
+end behavior;
+
+
+
+
+
+
 
